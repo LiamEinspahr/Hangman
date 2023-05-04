5b3ce88edcab25ec263c79017f135fd72df2cffe
liam.wes <57922825+LiamEinspahr@users.noreply.github.com>
2023-05-04 15:52:05 -0500

    Structured a new Player Class to organize player related data.
    Added proper properties for data manipulation of Player data.
    
    Added a feature to display the Player's guessed letters and score at the end of the game.

---
+++ Hangman/Player.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hangman
+{
+    internal class Player
+    {
+        private string name = "";
+        private List<char> uniqueGuesses = new List<char>();
+        private int score = 0;
+
+        public Player(string name)
+        {
+            this.name = name;
+        }
+
+        public List<char> getGuesses()
+        {
+            return (this.uniqueGuesses);
+        }
+
+        /* public string getName()
+         {
+             return (this.name);
+         }*/
+
+        public string Name {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+        public void setList(char c)
+        {
+            this.uniqueGuesses.Add(c);
+        }
+
+        public int getListCount()
+        {
+            return this.uniqueGuesses.Count; ;
+        }
+
+        public bool exists(char c)
+        {
+            if (this.uniqueGuesses.Contains(c))
+                return true;
+            return false;
+        }
+
+        public char getIndex(int index)
+        {
+            char tmp = this.uniqueGuesses[index];
+            Debug.WriteLine("Current: " + tmp);
+            return (tmp);
+        }
+
+        /*public void setScore(int pt)
+        {
+            this.score = this.score + pt;
+        }*/
+
+        /*public int getScore()
+        {
+            return (this.score);
+        }*/
+
+        public int Score
+        {
+            get { return (this.score); }
+            set { this.score = value; }
+        }
+
+    }
+}
--- Hangman/Program.cs
+++ Hangman/Program.cs
@@ -1,9 +1,18 @@
-﻿namespace Hangman
+﻿using Microsoft.VisualBasic.FileIO;
+using System.Numerics;
+using System.Reflection;
+
+namespace Hangman
 {
     internal class Program
     {
-        static string? name;
+        
+        static string correctWord = "hangman";
         static int numberOfGuesses = 0;
+        static char[] letters;
+        static Player player;
+        
+
         static void Main(string[] args)
         {
             StartGame();
@@ -13,39 +22,116 @@
         }
         private static void StartGame()
         {
-            Console.WriteLine("Starting the game...");
+            letters = new char[correctWord.Length];
+            for(int i = 0; i < letters.Length; i++)
+                letters[i] = '-';
+
             AskForUserName();
         }
 
         static void AskForUserName()
         {
-            Console.WriteLine("Enter your name:");
-            name = Console.ReadLine();
+                Console.WriteLine("Enter your name: ");
+                string? name = Console.ReadLine();
+                if (name.Length <= 1)
+                {
+                    Console.WriteLine("Invalid name length...please try again\n");
+                    AskForUserName();
+                }
+            player = new Player(name);
         }
 
         static void PlayGame()
         {
-            Console.WriteLine("Playing the game...");
-            DisplayMaskedWord();
-            AskForLetter();
+            do
+            {
+                Console.Clear();
+                DisplayMaskedWord();
+                char guessedLetter = AskForLetter();
+                CheckLetter(guessedLetter);
+            } while (correctWord != new string(letters));
+        }
+
+        private static void CheckLetter(char guessedLetter)
+        {
+            bool addPoint = false;
+
+            for(int i = 0; i < correctWord.Length; i++)
+            {
+                if (guessedLetter == correctWord[i])
+                {
+                    if(addPoint == false)
+                        addPoint = true;
+                    letters[i] = guessedLetter;
+                }
+                
+            }
+
+            if (addPoint)
+                player.Score += 1;
+
         }
 
         static void DisplayMaskedWord()
         {
-            Console.WriteLine("Displaying masked word...");
+            foreach (char c in letters)
+                Console.Write(c);
+
+            Console.WriteLine(); 
         }
 
-        static void AskForLetter()
+        static char AskForLetter()
         {
-            Console.WriteLine("Asking for letter...");
+            string input;
+            do 
+            { 
+                Console.Write("Enter a letter: ");
+                input = Console.ReadLine();
+            } while (input.Length != 1);
+
+            char temp = input[0];
+            CheckAddUnique(temp);
             numberOfGuesses++;
+
+            return input[0];
         }
 
         static void EndGame()
         {
             Console.WriteLine("Game over...");
-            Console.WriteLine($"Thank you for playing {name}");
+            Console.WriteLine($"Thank you for playing {player.Name}");
             Console.WriteLine($"Total number of guesses: {numberOfGuesses}");
+            Console.WriteLine($"Total score: {player.Score}");
+            DisplayUniqueGuesses();
+        }
+
+        private static void CheckAddUnique(char potentialToAdd)
+        {
+            if (player.getListCount() == 0 || player.exists(potentialToAdd) == false)
+            {
+                player.setList(potentialToAdd);
+            }
+                
+            return;
+        }
+        private static void DisplayUniqueGuesses()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Here are all of your unique guesses: ");
+            Console.Write("[");
+            for(int i = 0; i < player.getGuesses().Count; i++)
+            {
+                if (i == player.getListCount() - 1)
+                {
+                    Console.Write(player.getIndex(i));
+                }
+                else
+                {
+                    Console.Write(player.getIndex(i));
+                    Console.Write(' ');
+                }
+            }
+            Console.Write("]\n");
         }
     }
 }
